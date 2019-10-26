6823cf823ffd271f4ccfb8441edcbdc43de23616
Sang Lai <sanglai@lecle.co.kr>
2019-10-26 18:58:01 +0700

    Fixed bugs

--- js/circle-examples.js
+++ js/circle-examples.js
@@ -1,30 +1,30 @@
 $('.first.circle').circleProgress({
-    value: 0.39
+    value: 0.2
 }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(parseInt(39 * progress) + '<i>%</i>');
+    $(this).find('strong').html(parseInt(15 * progress) + '<i>%</i>');
 });
 $('.second.circle').circleProgress({
-    value: 0.45
+    value: 0.3
 }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(parseInt(45 * progress) + '<i>%</i>');
+    $(this).find('strong').html(parseInt(30 * progress) + '<i>%</i>');
 });
 $('.third.circle').circleProgress({
-    value: 0.45
+    value: 0.5
 }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(parseInt(40 * progress) + '<i>%</i>');
+    $(this).find('strong').html(parseInt(50 * progress) + '<i>%</i>');
 });
 $('.fourth.circle').circleProgress({
-    value: 0.55
+    value: 0.7
 }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(parseInt(55 * progress) + '<i>%</i>');
+    $(this).find('strong').html(parseInt(70 * progress) + '<i>%</i>');
 });
 $('.fifth.circle').circleProgress({
-    value: 0.45
+    value: 0.9
 }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(parseInt(45 * progress) + '<i>%</i>');
+    $(this).find('strong').html(parseInt(90 * progress) + '<i>%</i>');
 });
 $('.sixth.circle').circleProgress({
-    value: 0.37
+    value: 1
 }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(parseInt(37 * progress) + '<i>%</i>');
+    $(this).find('strong').html(parseInt(100 * progress) + '<i>%</i>');
 });
